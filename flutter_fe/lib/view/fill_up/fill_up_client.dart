e90119d9dc26ffc9859d270282c7674c0c3def7a
CoderCla <vegaclarence77@gmail.com>
2025-03-13 16:33:53 +0800

    add client verificaiton form

--- flutter_fe/lib/view/fill_up/fill_up_client.dart
+++ flutter_fe/lib/view/fill_up/fill_up_client.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
 import 'package:flutter_fe/controller/profile_controller.dart';
 import 'package:flutter_fe/view/sign_in/sign_in.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
-class FillUpTasker extends StatefulWidget {
-  const FillUpTasker({super.key});
+class FillUpClient extends StatefulWidget {
+  const FillUpClient({super.key});
 
   @override
-  State<FillUpTasker> createState() => _FillUpTaskerState();
+  State<FillUpClient> createState() => _FillUpClientState();
 }
 
-class _FillUpTaskerState extends State<FillUpTasker> {
+class _FillUpClientState extends State<FillUpClient> {
   int currentStep = 0;
 
   final ProfileController _controller = ProfileController();
@@ -20,10 +21,26 @@ class _FillUpTaskerState extends State<FillUpTasker> {
   String? _fileName; // Store the selected file name
   File? _selectedImage; // Store the selected image
   String? _imageName; // Store the selected image name
+  File? _selectedImageID; // Store the selected image
+  String? _imageNameID;
   String? selectedGender;
   List<String> genderOptions = ["Male", "Female"];
   List<String> specializtion = ['Tech Support', 'Cleaning', 'Plumbing'];
   String? selectedSpecialization;
+
+  final ImagePicker _picker = ImagePicker();
+  XFile? _imageFile; // Store the picked image
+
+  // Function to pick an image from gallery
+  Future<void> _pickImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = pickedFile;
+      });
+    }
+  }
 
   Future<void> _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -40,7 +57,7 @@ class _FillUpTaskerState extends State<FillUpTasker> {
     }
   }
 
-  Future<void> _pickImage() async {
+  Future<void> _pickImageProfile() async {
     final ImagePicker picker = ImagePicker();
     final XFile? pickedFile = await picker.pickImage(
       source:
@@ -55,6 +72,21 @@ class _FillUpTaskerState extends State<FillUpTasker> {
     }
   }
 
+  Future<void> _pickImageID() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(
+      source:
+          ImageSource.gallery, // Change to ImageSource.camera for camera input
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImageID = File(pickedFile.path); // Store the selected image
+        _imageNameID = pickedFile.name; // Store image name
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,7 +97,7 @@ class _FillUpTaskerState extends State<FillUpTasker> {
           child: Column(
             children: [
               Text(
-                'Complete your service account',
+                'Verify',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     color: const Color(0xFF0272B1),
@@ -77,7 +109,7 @@ class _FillUpTaskerState extends State<FillUpTasker> {
                     left: 80, right: 80, top: 10, bottom: 10),
                 child: Text(
                   textAlign: TextAlign.center,
-                  "Provide service right away, create an account now!",
+                  "Get your account verified",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                 ),
               ),
@@ -177,11 +209,16 @@ class _FillUpTaskerState extends State<FillUpTasker> {
         Step(
             state: currentStep > 0 ? StepState.complete : StepState.indexed,
             isActive: currentStep >= 0,
-            title: Text('Basic'),
+            title: Text(''),
             content: Column(
               children: [
+                Text(
+                  'Complete your personal information!',
+                  style: GoogleFonts.openSans(
+                      fontSize: 20, color: Color(0xFF0272B1)),
+                ),
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
+                  padding: const EdgeInsets.only(bottom: 10, top: 30),
                   child: TextFormField(
                     controller: _controller.firstNameController,
                     cursorColor: Color(0xFF0272B1),
@@ -339,12 +376,65 @@ class _FillUpTaskerState extends State<FillUpTasker> {
         Step(
             state: currentStep > 1 ? StepState.complete : StepState.indexed,
             isActive: currentStep >= 1,
-            title: Text('Profile'),
+            title: Text(''),
             content: Column(
               children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Choose a profile for taskers to know you more!',
+                    style: GoogleFonts.openSans(
+                        fontSize: 20, color: Color(0xFF0272B1)),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          // Placeholder or selected image
+                          Container(
+                            width: 100,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300], // Gray placeholder
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: _imageFile == null
+                                ? Center(child: Text(''))
+                                : Image.file(
+                                    File(_imageFile!.path),
+                                    fit: BoxFit.cover,
+                                    width: 300,
+                                    height: 400,
+                                  ),
+                          ),
+                          // "+" Button
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: _pickImage,
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.red,
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
                 if (_selectedImage != null)
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
+                    padding: const EdgeInsets.only(bottom: 30.0),
                     child: CircleAvatar(
                       backgroundImage: FileImage(_selectedImage!),
                       radius: 70,
@@ -354,112 +444,44 @@ class _FillUpTaskerState extends State<FillUpTasker> {
                   style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10))),
-                  onPressed: _pickImage,
-                  child: Text("Select profile"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 10),
-                  child: TextFormField(
-                    maxLines: 2,
-                    controller: _controller.emailController,
-                    cursorColor: Color(0xFF0272B1),
-                    validator: (value) => value!.isEmpty ? "Bio..." : null,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0xFFF1F4FF),
-                        hintText: 'Bio...',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.transparent, width: 0),
-                            borderRadius: BorderRadius.circular(10)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                                color: Color(0xFF0272B1), width: 2))),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: TextFormField(
-                    maxLines: 3,
-                    controller: _controller.passwordController,
-                    cursorColor: Color(0xFF0272B1),
-                    validator: (value) =>
-                        value!.isEmpty ? "List of skills..." : null,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0xFFF1F4FF),
-                        hintText: 'List of skills...',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.transparent, width: 0),
-                            borderRadius: BorderRadius.circular(10)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                                color: Color(0xFF0272B1), width: 2))),
-                  ),
+                  onPressed: _pickImageProfile,
+                  child: Text("Select Profile"),
                 ),
 
                 // Show Selected Image Start (if any)
               ],
             )),
         Step(
-            isActive: currentStep >= 2,
-            title: Text('Certs'),
+            state: currentStep > 1 ? StepState.complete : StepState.indexed,
+            isActive: currentStep >= 1,
+            title: Text(''),
             content: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: DropdownButtonFormField<String>(
-                    value: selectedSpecialization,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0xFFF1F4FF),
-                        //labelText: 'Select an option',
-                        hintText: 'Specialization...',
-                        hintStyle: TextStyle(color: Color(0xFF0272B1)),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.transparent, width: 0),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide:
-                              BorderSide(color: Color(0xFF0272B1), width: 2),
-                        )),
-                    items: specializtion.map((String item) {
-                      return DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(item),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedSpecialization = newValue;
-                        // _controller.jobSpecializationController.text =
-                        //     newValue ?? "";
-                      });
-                    },
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Provide a professional ID so that we can verify it is really you!',
+                    style: GoogleFonts.openSans(
+                        fontSize: 20, color: Color(0xFF0272B1)),
                   ),
                 ),
+                if (_selectedImageID != null)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: CircleAvatar(
+                      backgroundImage: FileImage(_selectedImageID!),
+                      radius: 70,
+                    ),
+                  ),
                 ElevatedButton(
-                  onPressed: _pickFile,
-                  child: Text("Pick PDF"),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: _pickImageID,
+                  child: Text("Select ID"),
                 ),
-                // File Picker Button End
 
-                // Show Selected File Start (if any)
-                _selectedFile != null
-                    ? Column(
-                        children: [
-                          Text("Selected File: $_fileName"),
-                        ],
-                      )
-                    : Text("No File Selected"),
+                // Show Selected Image Start (if any)
               ],
             )),
       ];
