622eb34c30959b8262a17844398bd4e8f55f230c
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-03-19 15:52:01 +0800

    Fix some merge

--- flutter_fe/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_fe/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,6 @@ import file_picker
 import file_selector_macos
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
@@ -19,6 +17,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
