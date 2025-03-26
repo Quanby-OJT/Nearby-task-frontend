10d4864979504c41bf2250183c433167ace5f0a2
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-03-26 20:28:07 +0800

    Created pagination for the tables

--- angular_fe/src/app/services/report.service.ts
+++ angular_fe/src/app/services/report.service.ts
@@ -4,19 +4,17 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class ReportService {
+  private apiUrl = 'http://localhost:5000/connect';
+  constructor(private http: HttpClient){}
 
-    private apiUrl = 'http://localhost:5000/connect';
-    constructor(private http: HttpClient){}
-
-    getReport(): Observable<any>{
-        return this.http.get<any>(`${this.apiUrl}/getReports`).pipe(
-            tap(response => {
-                console.log('Response from backend:', response);
-            })
-        );
-    }
-
+  getReport(): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/getReports`).pipe(
+      tap(response => {
+        console.log('Response from backend:', response);
+      })
+    );
+  }
 }
