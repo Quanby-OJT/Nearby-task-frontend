214bea056f5add9ee4c35841d855362e724ce0e0
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-04-21 15:13:19 +0800

    Feedback Connections

--- angular_fe/src/app/modules/feedback-management/feedback/feedback.component.ts
+++ angular_fe/src/app/modules/feedback-management/feedback/feedback.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FeedbackService } from 'src/app/services/feedback.service';
 
 @Component({
   selector: 'app-feedback',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
+  feedbacks: any[] = [];
 
+  constructor(private feedbackService: FeedbackService) {}
+
+  ngOnInit(): void {
+    this.feedbackService.getFeedback().subscribe(
+      (response: any) => {
+        console.log('Received feedback data:', response);
+        this.feedbacks = response.feedbacks || [];
+      },
+      (error) => {
+        console.error('Error fetching feedbacks', error);
+      }
+    );
+  }
 }
