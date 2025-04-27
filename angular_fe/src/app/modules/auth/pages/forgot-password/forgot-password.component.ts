226c52ad1e4ccdb1dbb9b92f46d28934b825284d
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-04-27 15:58:54 +0800

    Adding Steppers

--- angular_fe/src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
+++ angular_fe/src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -7,10 +8,20 @@ import { ButtonComponent } from 'src/app/shared/components/button/button.compone
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css'],
-  imports: [FormsModule, RouterLink, ButtonComponent],
+  imports: [FormsModule, RouterLink, ButtonComponent, NgClass, CommonModule],
 })
 export class ForgotPasswordComponent implements OnInit {
+  currentStep: number = 1;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  nextStep() {
+    if (this.currentStep < 3) this.currentStep++;
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) this.currentStep--;
+  }
 }
