f2d6c7731a31c9d42785dde65b061b1f2eaedaa1
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-05-01 10:31:17 +0800

    some changes

--- angular_fe/src/app/modules/task-management/task/task.component.ts
+++ angular_fe/src/app/modules/task-management/task/task.component.ts
@@ -9,6 +9,7 @@ import autoTable from 'jspdf-autotable';
 import { saveAs } from 'file-saver';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-task',
