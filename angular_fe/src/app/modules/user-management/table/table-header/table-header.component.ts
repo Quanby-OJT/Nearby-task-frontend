4f2c48e539a119c174ef99bfaa67f07f52244d0e
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-05-04 23:48:45 +0800

    Fixes some bugs in sorting

--- angular_fe/src/app/modules/user-management/table/table-header/table-header.component.ts
+++ angular_fe/src/app/modules/user-management/table/table-header/table-header.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: '[app-table-header]',
-  imports: [AngularSvgIconModule],
+  imports: [AngularSvgIconModule, CommonModule],
   templateUrl: './table-header.component.html',
   styleUrl: './table-header.component.css',
 })
