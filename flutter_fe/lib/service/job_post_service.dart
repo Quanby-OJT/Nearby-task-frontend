16ddb25f4e58a93dfecee564f95ac521e1bddef5
Ronnie Estillero <ronniewarrior09@gmail.com>
2025-05-13 09:38:58 +0800

    payment UI

--- flutter_fe/lib/service/job_post_service.dart
+++ flutter_fe/lib/service/job_post_service.dart
@@ -14,7 +14,7 @@ import '../model/client_model.dart';
 import '../model/tasker_model.dart';
 
 class JobPostService {
-  static String url = apiUrl ?? "http://192.168.1.12:5000/connect";
+  static String url = apiUrl ?? "http://192.168.43.15:5000/connect";
   static final storage = GetStorage();
   static final token = storage.read('session');
 
