63dced39d3aebc152a71ebffd25cf41abca28d7c
Clarence Vega <vegaclarence77@gmail.com>
2025-05-19 07:36:57 +0800

    modify wallet ui

--- flutter_fe/lib/view/business_acc/home_page.dart
+++ flutter_fe/lib/view/business_acc/home_page.dart
@@ -419,81 +419,81 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                         overlayEntry.remove();
                       },
                     ),
-//                     if (_role == "Client") ...[
-//                       ListTile(
-//                         leading: Icon(
-//                           FontAwesomeIcons.coins,
-//                           color: const Color(0xFFB71A4A),
-//                         ),
-//                         title: Text(
-//                           'Manage Tokens',
-//                           style: GoogleFonts.poppins(
-//                             color: Colors.black,
-//                             fontSize: 14,
-//                             fontWeight: FontWeight.w300,
-//                           ),
-//                         ),
-//                         onTap: () {
-//                           if (_user?.user.accStatus?.toLowerCase() ==
-//                               'review') {
-//                             Navigator.push(
-//                               context,
-//                               MaterialPageRoute(
-//                                   builder: (context) => EscrowTokenScreen()),
-//                             );
-//                             overlayEntry.remove();
-//                             return;
-//                           }
+                    if (_role == "Client") ...[
+                      ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.coins,
+                          color: const Color(0xFFB71A4A),
+                        ),
+                        title: Text(
+                          'Manage Tokens',
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        onTap: () {
+                          if (_user?.user.accStatus?.toLowerCase() ==
+                              'review') {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => EscrowTokenScreen()),
+                            );
+                            overlayEntry.remove();
+                            return;
+                          }
 
-//                           if (_existingProfileImageUrl == null ||
-//                               _existingIDImageUrl == null ||
-//                               _existingProfileImageUrl!.isEmpty ||
-//                               _existingIDImageUrl!.isEmpty ||
-//                               !_documentValid) {
-//                             overlayEntry.remove();
-//                             _showWarningDialog();
-//                             return;
-//                           }
-//                           Navigator.push(
-//                             context,
-//                             MaterialPageRoute(
-//                                 builder: (context) => EscrowTokenScreen()),
-//                           );
-//                           overlayEntry.remove();
-//                         },
-//                       ),
-//                     ],
-                    // ListTile(
-                    //   leading: Icon(
-                    //     FontAwesomeIcons.coins,
-                    //     color: const Color(0xFF03045E),
-                    //   ),
-                    //   title: Text(
-                    //     'Tokens',
-                    //     style: GoogleFonts.poppins(
-                    //       color: const Color(0xFF03045E),
-                    //       fontSize: 14,
-                    //       fontWeight: FontWeight.w300,
-                    //     ),
-                    //   ),
-                    //   onTap: () {
-                    //     if (_existingProfileImageUrl == null ||
-                    //         _existingIDImageUrl == null ||
-                    //         _existingProfileImageUrl!.isEmpty ||
-                    //         _existingIDImageUrl!.isEmpty ||
-                    //         !_documentValid) {
-                    //       overlayEntry.remove();
-                    //       _showWarningDialog();
-                    //       return;
-                    //     }
-                    //     Navigator.push(
-                    //       context,
-                    //       MaterialPageRoute(
-                    //           builder: (context) => EscrowTokenScreen()),
-                    //     );
-                    //     overlayEntry.remove();
-                    //   },
-                    // ),
+                          if (_existingProfileImageUrl == null ||
+                              _existingIDImageUrl == null ||
+                              _existingProfileImageUrl!.isEmpty ||
+                              _existingIDImageUrl!.isEmpty ||
+                              !_documentValid) {
+                            overlayEntry.remove();
+                            _showWarningDialog();
+                            return;
+                          }
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EscrowTokenScreen()),
+                          );
+                          overlayEntry.remove();
+                        },
+                      ),
+                    ],
+                    ListTile(
+                      leading: Icon(
+                        FontAwesomeIcons.coins,
+                        color: const Color(0xFF03045E),
+                      ),
+                      title: Text(
+                        'Tokens',
+                        style: GoogleFonts.poppins(
+                          color: const Color(0xFF03045E),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      onTap: () {
+                        if (_existingProfileImageUrl == null ||
+                            _existingIDImageUrl == null ||
+                            _existingProfileImageUrl!.isEmpty ||
+                            _existingIDImageUrl!.isEmpty ||
+                            !_documentValid) {
+                          overlayEntry.remove();
+                          _showWarningDialog();
+                          return;
+                        }
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EscrowTokenScreen()),
+                        );
+                        overlayEntry.remove();
+                      },
+                    ),
                     ListTile(
                       leading: Icon(
                         Icons.help,
