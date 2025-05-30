9f0b03b1adc883b767a6ffa16d4e0f4d41bb62da
Pogs789 <ceslorica15@gmail.com>
2025-05-30 10:36:06 +0800

    update auth user model

--- flutter_fe/lib/model/auth_user.dart
+++ flutter_fe/lib/model/auth_user.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_fe/model/tasker_model.dart';
 import '../model/user_model.dart';
+import 'client_model.dart';
 
 class AuthenticatedUser {
   final UserModel user;
+  final TaskerModel? tasker;
+  final ClientModel? client;
   final bool isClient;
   final bool isTasker;
 
@@ -9,6 +13,8 @@ class AuthenticatedUser {
     required this.user,
     this.isClient = false,
     this.isTasker = false,
+    this.tasker,
+    this.client,
   });
 
   @override
