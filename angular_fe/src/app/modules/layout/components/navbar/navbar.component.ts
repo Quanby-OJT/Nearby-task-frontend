6117422adf1a44d63c96f7718895c22fe88199b4
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-06-13 10:35:07 +0800

    Responsive Nav
    fix the broken viewport in sm

--- angular_fe/src/app/modules/layout/components/navbar/navbar.component.ts
+++ angular_fe/src/app/modules/layout/components/navbar/navbar.component.ts
@@ -8,12 +8,13 @@ import { SessionLocalStorage } from 'src/services/sessionStorage';
 import { interval, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { Users } from 'src/model/user-management';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  imports: [AngularSvgIconModule, NavbarMenuComponent, ProfileMenuComponent, NavbarMobileComponent],
+  imports: [AngularSvgIconModule, NavbarMenuComponent, ProfileMenuComponent, NavbarMobileComponent, NgClass],
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   currentDateTime: string = '';
